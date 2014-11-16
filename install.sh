36e551bbb1c0bc316aaf5a37ce3b3b19ac0aac12
Andrew Roetker <andrew.roetker@puppetlabs.com>
2014-11-16 12:30:20 -0800

    copy .vim instead of linking

--- install.sh
+++ install.sh
@@ -3,20 +3,28 @@
 pushd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null
 dir=`pwd`
 for line in `ls -la | awk '/ \.+/ { print $9 }'`; do
-    if [ $line != '.' -a $line != '..' -a $line != '.git' ]; then
+    if [ $line == '.vim' ]; then
+        rm -rf $HOME/.vim
+        cp -r $dir/.vim $HOME/.vim
+        mkdir -p $HOME/.vim/bundle
+        pushd "${HOME}/.vim/bundle" &> /dev/null
+            git clone https://github.com/gmarik/Vundle.vim.git
+        popd &> /dev/null
+        vim +PluginInstall +qall
+    elif [ $line != '.' -a $line != '..' -a $line != '.git' ]; then
         ln -fs "${dir}/$line" "${HOME}/${line}"
     fi
 done
 if [ "$(which brew)" != "brew not found" ]; then
-    brew update
-    if [ -n "$(brew ls --versions go)" ]; then
+    if [ -z "$(brew ls --versions go)" ]; then
         brew install go
     fi
-    if [ -n "$(brew ls --versions rbenv)" ]; then
+    if [ -z "$(brew ls --versions rbenv)" ]; then
         # Remember to setup default ruby
         brew install rbenv ruby-build
     fi
-    if [ -n "$(brew ls --versions boot2docker)" ]; then
+    if [ -z "$(brew ls --versions boot2docker)" ]; then
         brew install boot2docker
     fi
+fi
 popd &> /dev/null
